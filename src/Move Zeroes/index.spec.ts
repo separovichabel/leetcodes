995b46da4f5db34b930249574a3cc08b20055719
Abel Separovich <separovi@gmail.com>
2023-02-11 23:17:20 -0300

    ajustando testes do commit anterior

--- src/Move Zeroes/index.spec.ts
+++ src/Move Zeroes/index.spec.ts
@@ -5,17 +5,17 @@ describe('moveZeroes', () => {
     it('first moveZeroes', () => {
         const expectedResult = [1,3,12,0,0]
         const arr = [0,1,0,3,12]
-        const resp = moveZeroes(arr)
+        moveZeroes(arr)
         
-        expect(resp).toEqual(expectedResult)
+        expect(arr).toEqual(expectedResult)
     })
 
     it('second moveZeroes', () => {
         const expectedResult = [0]
         const arr = [0]
-        const resp = moveZeroes(arr)
+        moveZeroes(arr)
         
-        expect(resp).toEqual(expectedResult)
+        expect(arr).toEqual(expectedResult)
     })
     
 });
