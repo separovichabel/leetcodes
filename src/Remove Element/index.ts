8a7ea9bd3eeb2d9ad5510b04fc9d8b5adf883fea
Abel Separovich <separovi@gmail.com>
2023-02-11 23:36:14 -0300

    alterando implementação Remove Element

--- src/Remove Element/index.ts
+++ src/Remove Element/index.ts
@@ -1,17 +1,31 @@
 export function removeElement(nums: number[], val: number): number {
-    let cursor = 0
-    let nextPosition = 0
-    let totalLength = nums.length
-
-    while (cursor < nums.length) {
-        if (nums[cursor] === val) {
-            cursor++
-            totalLength--
+    let insert = 0
+    let length = nums.length
+    for (let read = 0; read < nums.length; read++) {
+        if (nums[read] === val) {
+            length--
             continue
         }
-        nums[nextPosition] = nums[cursor]
-        nextPosition++
-        cursor++
+        nums[insert] = nums[read]
+        insert++
     }
-    return totalLength
+    return length
 };
+
+// export function removeElement(nums: number[], val: number): number {
+//     let cursor = 0
+//     let nextPosition = 0
+//     let totalLength = nums.length
+
+//     while (cursor < nums.length) {
+//         if (nums[cursor] === val) {
+//             cursor++
+//             totalLength--
+//             continue
+//         }
+//         nums[nextPosition] = nums[cursor]
+//         nextPosition++
+//         cursor++
+//     }
+//     return totalLength
+// };
