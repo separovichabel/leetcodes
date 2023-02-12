1bb0492c1f6804a8230eae27f5151b418df4b114
Abel Separovich <separovi@gmail.com>
2023-02-11 22:47:25 -0300

    reduzindo o algoritimo remove duplicates

--- src/Remove Duplicates from Sorted Array/index.ts
+++ src/Remove Duplicates from Sorted Array/index.ts
@@ -1,26 +1,14 @@
 export function removeDuplicates(nums: number[]): number {
-    let cursor = 0
-    let cursorInsert = 0
     let totalLength = nums.length
-
-    let lastElement = -101 // less than the minimum value possible
-
-    let curNum: number;
-    while (cursor < nums.length) {
-        curNum = nums[cursor]
-
-        if (lastElement >= curNum) {
-            cursor++
-            totalLength--
+    let insertIndex = 1
+    for (let readIndex = 1; readIndex < nums.length; readIndex++) {
+        if (nums[readIndex - 1] != nums[readIndex]) {
+            nums[insertIndex] = nums[readIndex]
+            insertIndex++
             continue
         }
 
-        lastElement = curNum
-        nums[cursorInsert] = curNum
-
-        cursorInsert++
-        cursor++
+        totalLength--
     }
-
     return totalLength
 };
